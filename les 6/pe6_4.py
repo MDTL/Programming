72612e857226e60e64bb5d19bea1b4778ec9df00
MDTL <Mitchell.tlam@student,hu.nl>
2017-09-25 15:16:02 +0200

    les 6

---
+++ les 6/pe6_4.py
@@ -0,0 +1,36 @@
+studentencijfers = [[95, 92, 86], [66, 75, 54], [89, 72, 100], [34, 0, 0]]
+
+def gemiddelde_per_student(studentcijfers):
+
+    antw = []
+
+    for i in range(len(studentcijfers)):
+
+        length = len(studentcijfers[i])
+
+        som = sum(studentcijfers[i]) / length
+
+        gem = round(som)
+
+        antw.append(gem)
+
+    return antw
+
+def gemiddelde_van_alle_studenten(studentcijfers):
+
+    som = 0
+    count = 0
+
+    for row in studentcijfers:
+        for elem in row:
+            som = som + elem
+            count = count + 1
+            antw = som / count
+
+    antw = round(antw)
+
+    return antw
+
+
+print(gemiddelde_per_student(studentencijfers))
+print(gemiddelde_van_alle_studenten(studentencijfers))
